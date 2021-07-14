aef6cca0821a0ad2082c45f464fd5d293d039e07
nightwishxp <115199072+nightwishxp@users.noreply.github.com>
2021-07-14 20:27:58 +0000

    update nehan.js

--- examples/schema.rs
+++ examples/schema.rs
@@ -3,4 +3,9 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use secret_sec
+use secret_secret::msg::{HandleAnswer, HandleMsg, InitMsg, QueryAnswer, QueryMsg};
+
+fn main() {
+    let mut out_dir = current_dir().unwrap();
+    out_dir.push("schema");
+    create_dir_all(&out_dir).unwra
