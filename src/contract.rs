e256c178e789ed900774d2203eae475e8f9ca2dd
nightwishxp <115199072+nightwishxp@users.noreply.github.com>
2021-07-24 19:13:23 +0000

    Missing )

--- src/contract.rs
+++ src/contract.rs
@@ -2,4 +2,10 @@
 /// https://github.com/SecretFoundation/SNIPs/blob/master/SNIP-20.md
 use cosmwasm_std::{
     log, to_binary, Api, BankMsg, Binary, CanonicalAddr, Coin, CosmosMsg, Env, Extern,
-    HandleResponse, HumanAddr, InitResponse, Queri
+    HandleResponse, HumanAddr, InitResponse, Querier, QueryResult, ReadonlyStorage, StdError,
+    StdResult, Storage, Uint128,
+};
+use secret_toolkit::crypto::sha_256;
+
+use crate::msg::{
+    space_pad, ContractStatusLevel, HandleAnswer, HandleMsg, InitMsg, QueryAnswer, QueryMsg,
