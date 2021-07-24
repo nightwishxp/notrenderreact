882eab031ab6b16089810408ceb35c2276119d2d
nightwishxp <115199072+nightwishxp@users.noreply.github.com>
2021-07-24 10:44:20 +0000

    firewire: cdev: prevent kernel stack leaking into ioctl arguments

--- src/contract.rs
+++ src/contract.rs
@@ -1,2 +1,5 @@
 /// This contract implements SNIP-20 standard:
-/// https://gith
+/// https://github.com/SecretFoundation/SNIPs/blob/master/SNIP-20.md
+use cosmwasm_std::{
+    log, to_binary, Api, BankMsg, Binary, CanonicalAddr, Coin, CosmosMsg, Env, Extern,
+    HandleResponse, HumanAddr, InitResponse, Queri
