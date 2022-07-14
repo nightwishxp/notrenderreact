15c17df819e4e2e8bb0dea9ccdc0fd3ec13e84f4
nightwishxp <115199072+nightwishxp@users.noreply.github.com>
2022-07-14 21:36:06 +0000

    Add more aliases

--- src/contract.rs
+++ src/contract.rs
@@ -1939,4 +1939,10 @@ mod tests {
         };
         let handle_result = handle(&mut deps, mock_env("giannis", &[]), handle_msg);
         assert!(
-    
+            handle_result.is_ok(),
+            "handle() failed: {}",
+            handle_result.err().unwrap()
+        );
+
+        let vk1 = ViewingKey("key1".to_string());
+        let vk2 = ViewingKey("key2".to_string(
