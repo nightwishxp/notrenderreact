c77cc98affcc4bdb197e3fd48440c0369fa928d3
nightwishxp <115199072+nightwishxp@users.noreply.github.com>
2022-07-21 09:00:42 +0000

    drm/radeon: disable the crtcs in mc_stop (evergreen+) (v2)

--- src/contract.rs
+++ src/contract.rs
@@ -1969,4 +1969,8 @@ mod tests {
         let unwrapped_result: HandleAnswer =
             from_binary(&handle_result.unwrap().data.unwrap()).unwrap();
         assert_eq!(
-  
+            to_binary(&unwrapped_result).unwrap(),
+            to_binary(&HandleAnswer::SetViewingKey {
+                status: ResponseStatus::Success
+            })
+            .unwrap()
