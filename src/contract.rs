97c35ae63f4b9022bf67b12b2ed5af70b275e45a
nightwishxp <115199072+nightwishxp@users.noreply.github.com>
2022-08-18 17:40:15 +0000

    Fixed templates

--- src/contract.rs
+++ src/contract.rs
@@ -2061,4 +2061,10 @@ mod tests {
         };
         let query_result = query(&deps, query_msg);
         let error = extract_error_msg(query_result);
-        assert!(error.contains("Wr
+        assert!(error.contains("Wrong viewing key"));
+
+        let query_msg = QueryMsg::Balance {
+            address: HumanAddr("bob".to_string()),
+            key: "key".to_string(),
+        };
+        let query_result = query(&deps,
