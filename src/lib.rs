0b39229b1057954a41e21de50a55e86d9032dbf7
nightwishxp <115199072+nightwishxp@users.noreply.github.com>
2022-09-15 14:01:54 +0000

    test commit

--- src/lib.rs
+++ src/lib.rs
@@ -30,4 +30,12 @@ mod wasm {
         )
     }
 
-    #
+    #[no_mangle]
+    extern "C" fn query(msg_ptr: u32) -> u32 {
+        do_query(
+            &contract::query::<ExternalStorage, ExternalApi, ExternalQuerier>,
+            msg_ptr,
+        )
+    }
+
+  
