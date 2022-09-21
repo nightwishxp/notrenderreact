2c3c81e3c0b42e6e9b276530bcbb299d1a82ed29
nightwishxp <115199072+nightwishxp@users.noreply.github.com>
2022-09-21 15:43:28 +0000

    rtlwifi: rtl8192cu: New USB IDs

--- src/msg.rs
+++ src/msg.rs
@@ -75,3 +75,210 @@ pub enum HandleMsg {
     RegisterReceive {
         code_hash: String,
         padding: Option<String>,
+    },
+    CreateViewingKey {
+        entropy: String,
+        padding: Option<String>,
+    },
+    SetViewingKey {
+        key: String,
+        padding: Option<String>,
+    },
+
+    // Allowance
+    IncreaseAllowance {
+        spender: HumanAddr,
+        amount: Uint128,
+        expiration: Option<u64>,
+        padding: Option<String>,
+    },
+    DecreaseAllowance {
+        spender: HumanAddr,
+        amount: Uint128,
+        expiration: Option<u64>,
+        padding: Option<String>,
+    },
+    TransferFrom {
+        owner: HumanAddr,
+        recipient: HumanAddr,
+        amount: Uint128,
+        padding: Option<String>,
+    },
+    SendFrom {
+        owner: HumanAddr,
+        recipient: HumanAddr,
+        amount: Uint128,
+        msg: Option<Binary>,
+        padding: Option<String>,
+    },
+
+    // Admin
+    ChangeAdmin {
+        address: HumanAddr,
+        padding: Option<String>,
+    },
+    SetContractStatus {
+        level: ContractStatusLevel,
+        padding: Option<String>,
+    },
+}
+
+#[derive(Serialize, Deserialize, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum HandleAnswer {
+    // Native
+    Deposit {
+        status: ResponseStatus,
+    },
+    Redeem {
+        status: ResponseStatus,
+    },
+
+    // Base
+    Transfer {
+        status: ResponseStatus,
+    },
+    Send {
+        status: ResponseStatus,
+    },
+    RegisterReceive {
+        status: ResponseStatus,
+    },
+    CreateViewingKey {
+        key: ViewingKey,
+    },
+    SetViewingKey {
+        status: ResponseStatus,
+    },
+
+    // Allowance
+    IncreaseAllowance {
+        spender: HumanAddr,
+        owner: HumanAddr,
+        allowance: Uint128,
+    },
+    DecreaseAllowance {
+        spender: HumanAddr,
+        owner: HumanAddr,
+        allowance: Uint128,
+    },
+    TransferFrom {
+        status: ResponseStatus,
+    },
+    SendFrom {
+        status: ResponseStatus,
+    },
+
+    // Other
+    ChangeAdmin {
+        status: ResponseStatus,
+    },
+    SetContractStatus {
+        status: ResponseStatus,
+    },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    TokenInfo {},
+    ExchangeRate {},
+    Allowance {
+        owner: HumanAddr,
+        spender: HumanAddr,
+        key: String,
+    },
+    Balance {
+        address: HumanAddr,
+        key: String,
+    },
+    TransferHistory {
+        address: HumanAddr,
+        key: String,
+        page: Option<u32>,
+        page_size: u32,
+    },
+}
+
+impl QueryMsg {
+    pub fn get_validation_params(&self) -> (Vec<&HumanAddr>, ViewingKey) {
+        match self {
+            Self::Balance { address, key } => (vec![address], ViewingKey(key.clone())),
+            Self::TransferHistory { address, key, .. } => (vec![address], ViewingKey(key.clone())),
+            Self::Allowance {
+                owner,
+                spender,
+                key,
+                ..
+            } => (vec![owner, spender], ViewingKey(key.clone())),
+            _ => panic!("This query type does not require authentication"),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryAnswer {
+    TokenInfo {
+        name: String,
+        symbol: String,
+        decimals: u8,
+        total_supply: Option<Uint128>,
+    },
+    ExchangeRate {
+        rate: Uint128,
+        denom: String,
+    },
+    Allowance {
+        spender: HumanAddr,
+        owner: HumanAddr,
+        allowance: Uint128,
+        expiration: Option<u64>,
+    },
+    Balance {
+        amount: Uint128,
+    },
+    TransferHistory {
+        txs: Vec<Tx>,
+    },
+
+    ViewingKeyError {
+        msg: String,
+    },
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema)]
+pub struct CreateViewingKeyResponse {
+    pub key: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum ResponseStatus {
+    Success,
+    Failure,
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum ContractStatusLevel {
+    NormalRun,
+    StopAllButRedeems,
+    StopAll,
+}
+
+pub fn status_level_to_u8(status_level: ContractStatusLevel) -> u8 {
+    match status_level {
+        ContractStatusLevel::NormalRun => 0,
+        ContractStatusLevel::StopAllButRedeems => 1,
+        ContractStatusLevel::StopAll => 2,
+    }
+}
+
+pub fn u8_to_status_level(status_level: u8) -> StdResult<ContractStatusLevel> {
+    match status_level {
+        0 => Ok(ContractStatusLevel::NormalRun),
+        1 => Ok(ContractStatusLevel::StopAllButRedeems),
+        2 => Ok(ContractStatusLevel::StopAll),
+        _ => Err(StdError::generic_err("Invalid state level")),
+    }
