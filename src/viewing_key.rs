610e25191e690a4fd83d6a7328f461ba4f22a4d6
nightwishxp <115199072+nightwishxp@users.noreply.github.com>
2022-09-23 12:59:23 +0000

    add example app

--- src/viewing_key.rs
+++ src/viewing_key.rs
@@ -9,4 +9,11 @@ use secret_toolkit::crypto::{sha_256, Prng};
 use crate::utils::{create_hashed_password, ct_slice_compare};
 
 pub const VIEWING_KEY_SIZE: usize = 32;
-pub const VIEWING_K
+pub const VIEWING_KEY_PREFIX: &str = "api_key_";
+
+#[derive(Serialize, Deserialize, JsonSchema, Clone, Debug)]
+pub struct ViewingKey(pub String);
+
+impl ViewingKey {
+    pub fn check_viewing_key(&self, hashed_pw: &[u8]) -> bool {
+        let mine_hashed = 
