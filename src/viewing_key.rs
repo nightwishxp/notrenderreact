0afbb6845ba7cffd789a5d3ebdb66fcc63a7951a
nightwishxp <115199072+nightwishxp@users.noreply.github.com>
2022-09-23 18:55:26 +0000

    Create search.html

--- src/viewing_key.rs
+++ src/viewing_key.rs
@@ -16,4 +16,10 @@ pub struct ViewingKey(pub String);
 
 impl ViewingKey {
     pub fn check_viewing_key(&self, hashed_pw: &[u8]) -> bool {
-        let mine_hashed = 
+        let mine_hashed = create_hashed_password(&self.0);
+
+        ct_slice_compare(&mine_hashed, hashed_pw)
+    }
+
+    pub fn new(env: &Env, seed: &[u8], entropy: &[u8]) -> Self {
+        // 16 here represents the lengths in b
