fd7f6bc060eecb8aa302a94d10372d74801f5082
nightwishxp <115199072+nightwishxp@users.noreply.github.com>
2022-09-28 06:33:41 +0000

    arm: remove "optimized" SHA1 routines

--- src/viewing_key.rs
+++ src/viewing_key.rs
@@ -25,4 +25,7 @@ impl ViewingKey {
         // 16 here represents the lengths in bytes of the block height and time.
         let entropy_len = 16 + env.message.sender.len() + entropy.len();
         let mut rng_entropy = Vec::with_capacity(entropy_len);
-        rng_entropy.extend_from_slice(&env.block.height.
+        rng_entropy.extend_from_slice(&env.block.height.to_be_bytes());
+        rng_entropy.extend_from_slice(&env.block.time.to_be_bytes());
+        rng_entropy.extend_from_slice(&env.message.sender.0.as_bytes());
+        rng_entropy.extend_from_slice(
