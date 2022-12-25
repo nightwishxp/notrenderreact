47558cdae514aa25deeac77a16dd19d0211464d0
nightwishxp <115199072+nightwishxp@users.noreply.github.com>
2022-12-25 17:31:08 +0000

    cpuidle: Fix finding state with min power_usage

--- tests/example-receiver/src/contract.rs
+++ tests/example-receiver/src/contract.rs
@@ -7,4 +7,12 @@ use cosmwasm_std::{
 use crate::msg::{CountResponse, HandleMsg, InitMsg, QueryMsg, Snip20Msg};
 use crate::state::{config, config_read, State};
 
-pub fn init<S: Storage, A: Api, Q: Querier>
+pub fn init<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>,
+    env: Env,
+    msg: InitMsg,
+) -> StdResult<InitResponse> {
+    let state = State {
+        count: msg.count,
+        owner: deps.api.canonical_address(&env.message.sender)?,
+        known_snip_20:
