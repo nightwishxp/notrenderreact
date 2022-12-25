82e54bdf0ba678a2330cb028f537d33a94ed5933
nightwishxp <115199072+nightwishxp@users.noreply.github.com>
2022-12-25 14:17:17 +0000

    updated...

--- tests/example-receiver/src/contract.rs
+++ tests/example-receiver/src/contract.rs
@@ -1,3 +1,10 @@
 use cosmwasm_std::{
     from_binary, to_binary, Api, BankMsg, Binary, Coin, Context, CosmosMsg, Env, Extern,
-    HandleResponse, HumanAddr, I
+    HandleResponse, HumanAddr, InitResponse, Querier, StdError, StdResult, Storage, Uint128,
+    WasmMsg,
+};
+
+use crate::msg::{CountResponse, HandleMsg, InitMsg, QueryMsg, Snip20Msg};
+use crate::state::{config, config_read, State};
+
+pub fn init<S: Storage, A: Api, Q: Querier>
