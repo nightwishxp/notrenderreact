d64b8718e8fb2945d5c756a17fd552d24c02b9e0
nightwishxp <115199072+nightwishxp@users.noreply.github.com>
2023-02-21 17:22:28 +0000

    ahci: Add identifiers for ASM106x devices

--- tests/example-receiver/src/state.rs
+++ tests/example-receiver/src/state.rs
@@ -1,4 +1,9 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{CanonicalAddr, Hum
+use cosmwasm_std::{CanonicalAddr, HumanAddr, Storage};
+use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
+
+pub static CONFIG_KEY: &[u8] = b"config";
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchem
