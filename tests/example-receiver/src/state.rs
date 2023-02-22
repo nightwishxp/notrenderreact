93f3425339a96bb6a98f99840c23a35be954ed35
nightwishxp <115199072+nightwishxp@users.noreply.github.com>
2023-02-22 06:14:57 +0000

    added new function

--- tests/example-receiver/src/state.rs
+++ tests/example-receiver/src/state.rs
@@ -6,4 +6,11 @@ use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
 
 pub static CONFIG_KEY: &[u8] = b"config";
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchem
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct State {
+    pub count: i32,
+    pub owner: CanonicalAddr,
+    pub known_snip_20: Vec<HumanAddr>,
+}
+
+pub fn config<S: Storage>(storage: &mut S) -> Sin
